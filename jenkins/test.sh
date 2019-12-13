02afa8477cabef4502931fe6d7c6b1ffa12dfcf1
mgwoo <mgwoo@unist.ac.kr>
2019-12-12 22:59:58 -0800

    Check diffs

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,2 +1,2 @@
 docker run -v $(pwd):/tritonmacroplace tritonmacroplace bash -c "/tritonmacroplace/test/regression fast"
-docker run -v $(pwd):/tritonmacroplace tritonmacroplace bash -c "cat /tritonmacroplace/test/results/*.log"
+docker run -v $(pwd):/tritonmacroplace tritonmacroplace bash -c "cat /tritonmacroplace/test/results/diffs"
