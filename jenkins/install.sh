fc4bdd78b2eb79eaed48790deb111bc09ef41213
mgwoo <mwoo@eng.ucsd.edu>
2019-12-16 17:23:14 -0800

    Update jenkins dockerfile with centos7 (tested on mac)

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,4 +1,4 @@
 mkdir -p /tritonmacroplace/build
 cd /tritonmacroplace/build
-cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBOOST_ROOT=/opt/rh/rh-mongodb32/root/usr -DSWIG_EXECUTABLE=/usr/local/bin/swig ..
-make
+cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBUILD_PYTHON=0 -DBUILD_TCL=0 ..
+make -j6
