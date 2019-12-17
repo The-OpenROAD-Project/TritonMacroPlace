74603edfa76fc25a2e56e2fa01099d272d74571d
mgwoo <mgwoo@unist.ac.kr>
2019-12-16 20:50:53 -0800

    Preserve weird gcc settings.

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,4 +1,4 @@
 mkdir -p /tritonmacroplace/build
 cd /tritonmacroplace/build
-cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBUILD_PYTHON=0 -DBUILD_TCL=0 -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_C_COMPILER=/usr/bin/gcc ..
+cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBUILD_PYTHON=0 -DBUILD_TCL=0 ..
 make
