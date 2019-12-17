2583ddb3018959ea12d301ed2fecf9141ff97292
mgwoo <mgwoo@unist.ac.kr>
2019-12-16 21:12:35 -0800

    clear build directory

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,4 +1,5 @@
+rm -rf /tritonmacroplace/build
 mkdir -p /tritonmacroplace/build
 cd /tritonmacroplace/build
-cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBUILD_PYTHON=0 -DBUILD_TCL=0 -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-6/root/usr/bin/g++ -DCMAKE_C_COMPILER=/opt/rh/devtoolset-6/root/usr/bin/gcc ..
+cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBUILD_PYTHON=0 -DBUILD_TCL=0 ..
 make
