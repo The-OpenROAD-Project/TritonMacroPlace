6d67b4fa75e9463003abd1e49dfe79a8a0914c07
mgwoo <mgwoo@unist.ac.kr>
2019-12-16 21:09:53 -0800

    gcc

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,4 +1,4 @@
 mkdir -p /tritonmacroplace/build
 cd /tritonmacroplace/build
-cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBUILD_PYTHON=0 -DBUILD_TCL=0 ..
+cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBUILD_PYTHON=0 -DBUILD_TCL=0 -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-6/root/usr/bin/g++ -DCMAKE_C_COMPILER=/opt/rh/devtoolset-6/root/usr/bin/gcc ..
 make
