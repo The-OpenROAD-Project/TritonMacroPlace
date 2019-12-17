059bba0bc38b936b1e95af036343e1a1cf153af2
mgwoo <mgwoo@unist.ac.kr>
2019-12-16 16:04:55 -0800

    Change swig pointer in build script

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,4 +1,4 @@
 mkdir -p /tritonmacroplace/build
 cd /tritonmacroplace/build
-cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBOOST_ROOT=/opt/rh/rh-mongodb32/root/usr ..
+cmake -DCMAKE_INSTALL_PREFIX=/tritonmacroplace/build -DBOOST_ROOT=/opt/rh/rh-mongodb32/root/usr -DSWIG_EXECUTABLE=/usr/local/bin/swig ..
 make
