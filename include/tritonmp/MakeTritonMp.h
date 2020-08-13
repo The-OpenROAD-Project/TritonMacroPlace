4aa17fe1b44132311eac0cbf4769027e6f522609
mgwoo <mwoo@eng.ucsd.edu>
2020-08-12 18:02:17 -0700

    clean authors

--- include/tritonmp/MakeTritonMp.h
+++ include/tritonmp/MakeTritonMp.h
@@ -1,7 +1,4 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Authors: Mingyu Woo
-//          (Ph.D. Advisor: Andrew. B. Kahng)
-//
 // BSD 3-Clause License
 //
 // Copyright (c) 2020, The Regents of the University of California
